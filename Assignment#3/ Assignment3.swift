e80e971d027c5729eea64200d0ece777ec6c1956
Dasha Chastokolenko <dashachastokolenko@macbook-air-dasha.local>
2019-02-04 15:51:32 +0200

    task#1

---
+++ Assignment#3/ Assignment3.swift
@@ -0,0 +1,17 @@
+//
+//   Assignment3.swift
+//  Assignment#3
+//
+//  Created by Dasha Chastokolenko on 2/4/19.
+//  Copyright © 2019 Dasha Chastokolenko. All rights reserved.
+//
+
+import UIKit
+
+class Assignment3: NSObject {
+    
+    static func numberOfCharactersInName(_ myName: String) {
+        let numberOfCharacters = myName.count
+        print("In \"\(myName)\" number of characters is \( numberOfCharacters - 1)")
+    }
+}
